5b5226e485df7a9f2a57ed3a3822722cd1c8508c
tomi1710 <2294@holbertonschool.com>
2021-02-02 18:26:23 +0000

    fix

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,11 +1,6 @@
 #include "sort.h"
-
-/*
- * task 3-quick_sort.c
- * Made by Facundo Diaz to Holberton School
- * Date - 01/02/2021
- */
-#include "sort.h"
+void original_quick_sort(int *A, int lo, int hi, size_t size);
+int partition(int *A, int lo, int hi, size_t size);
 /**
  * quick_sort - sort function using quick sort algoritmh
  * @array: array received
@@ -69,77 +64,3 @@ int partition(int *A, int lo, int hi, size_t size)
 		print_array(A, size);
 	return (i);
 }
-
-/**
-int partition(int *A, int lo, int hi);
-void _swap(int a, int b, int *A);
-void quick_sortt(int *A, int lo, int hi);
-/**
- * quick_sort - calls quick_sortt
- * @array: array
- * @size: size of array
- *
-void quick_sort(int *array, size_t size)
-{
-	quick_sortt(array, 0, size - 1);
-}
-/**
- * quick_sortt - sorts using quick sort algorithm
- * @A: array
- * @lo: low
- * @hi: high
- *
-void quick_sortt(int *A, int lo, int hi)
-{
-	int p;
-
-	if (lo < hi)
-	{
-		p = partition(A, lo, hi);
-		quick_sortt(A, lo, p - 1);
-		quick_sortt(A, p + 1, hi);
-	}
-}
-/**
- * partition - sorts using quick sort algorithm
- * @A: array
- * @lo: low
- * @hi: high
- * Return: a
- *
-int partition(int *A, int lo, int hi)
-{
-	int pivot = A[hi], i = lo, j;
-
-	for (j = lo; j <= hi; j++)
-	{
-		if (A[j] < pivot)
-		{
-			_swap(i, j, A);
-			i = i + 1;
-		}
-	}
-	_swap(i, hi, A);
-	return (i);
-}
-/**
- * _swap - sorts using quick sort algorithm
- * @A: array
- * @b: position to swap
- * @a: position to swap
- *
-void _swap(int a, int b, int *A)
-{
-	int mem, i;
-
-	for (i = 0; A[i] != '\0'; i++)
-	{
-	}
-
-	mem = A[a];
-	A[a] = A[b];
-	A[b] = mem;
-	if (A[a] != A[b])
-		print_array(A, i);
-}
-*/
