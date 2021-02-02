75f264b05df2faab203f94d466d2593096b6d240
tomi1710 <2294@holbertonschool.com>
2021-02-02 18:15:41 +0000

    fix betty

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -3,11 +3,21 @@
 int partition(int *A, int lo, int hi);
 void _swap(int a, int b, int *A);
 void quick_sortt(int *A, int lo, int hi);
+/**
+ * quick_sort - calls quick_sortt
+ * @array: array
+ * @size: size of array
+ */
 void quick_sort(int *array, size_t size)
 {
 	quick_sortt(array, 0, size - 1);
 }
-
+/**
+ * quick_sortt - sorts using quick sort algorithm
+ * @A: array
+ * @lo: low
+ * @hi: high
+ */
 void quick_sortt(int *A, int lo, int hi)
 {
 	int p;
@@ -19,7 +29,12 @@ void quick_sortt(int *A, int lo, int hi)
 		quick_sortt(A, p + 1, hi);
 	}
 }
-
+/**
+ * partition - sorts using quick sort algorithm
+ * @A: array
+ * @lo: low
+ * @hi: high
+ */
 int partition(int *A, int lo, int hi)
 {
 	int pivot = A[hi], i = lo, j;
@@ -33,9 +48,14 @@ int partition(int *A, int lo, int hi)
 		}
 	}
 	_swap(i, hi, A);
-	return i;
+	return (i);
 }
-
+/**
+ * quick_sortt - sorts using quick sort algorithm
+ * @A: array
+ * @b: position to swap
+ * @a: position to swap
+ */
 void _swap(int a, int b, int *A)
 {
 	int mem, i;
