51bc91c663f163ece9ac2fa72f3193bb4e8a1ed8
tomi1710 <2294@holbertonschool.com>
2021-02-02 18:23:51 +0000

    fix

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,76 @@
 #include "sort.h"
 
+/*
+ * task 3-quick_sort.c
+ * Made by Facundo Diaz to Holberton School
+ * Date - 01/02/2021
+ */
+#include "sort.h"
+/**
+ * quick_sort - sort function using quick sort algoritmh
+ * @array: array received
+ * @size: size of array
+ */
+void quick_sort(int *array, size_t size)
+{
+	original_quick_sort(array, 0, size - 1, size);
+}
+
+/**
+ * original_quick_sort - original function of quick sort
+ * @A: array received
+ * @lo: menor indice
+ * @hi: mayor indice
+ * @size: size of array
+ */
+void original_quick_sort(int *A, int lo, int hi, size_t size)
+{
+	int p;
+
+	if (lo < hi)
+	{
+		p = partition(A, lo, hi, size);
+		original_quick_sort(A, lo, p - 1, size);
+		original_quick_sort(A, p + 1, hi, size);
+	}
+}
+
+/**
+ * partition - partition function
+ * @A: array received
+ * @lo: menor indice
+ * @hi: mayor indice
+ * @size: size of array
+ * Return: i
+ */
+int partition(int *A, int lo, int hi, size_t size)
+{
+	int pivot, aux, j, i;
+
+	pivot = A[hi];
+	i = lo;
+	j = lo;
+	for (; j <= hi; j++)
+	{
+		if (A[j] < pivot)
+		{
+			aux = A[j];
+			A[j] = A[i];
+			A[i] = aux;
+			if (i != j)
+				print_array(A, size);
+			i++;
+		}
+	}
+	aux = A[i];
+	A[i] = A[hi];
+	A[hi] = aux;
+	if (A[hi] != pivot)
+		print_array(A, size);
+	return (i);
+}
+
+/**
 int partition(int *A, int lo, int hi);
 void _swap(int a, int b, int *A);
 void quick_sortt(int *A, int lo, int hi);
@@ -7,18 +78,17 @@ void quick_sortt(int *A, int lo, int hi);
  * quick_sort - calls quick_sortt
  * @array: array
  * @size: size of array
- */
+ *
 void quick_sort(int *array, size_t size)
 {
-	if (size > 1)
-		quick_sortt(array, 0, size - 1);
+	quick_sortt(array, 0, size - 1);
 }
 /**
  * quick_sortt - sorts using quick sort algorithm
  * @A: array
  * @lo: low
  * @hi: high
- */
+ *
 void quick_sortt(int *A, int lo, int hi)
 {
 	int p;
@@ -36,7 +106,7 @@ void quick_sortt(int *A, int lo, int hi)
  * @lo: low
  * @hi: high
  * Return: a
- */
+ *
 int partition(int *A, int lo, int hi)
 {
 	int pivot = A[hi], i = lo, j;
@@ -57,7 +127,7 @@ int partition(int *A, int lo, int hi)
  * @A: array
  * @b: position to swap
  * @a: position to swap
- */
+ *
 void _swap(int a, int b, int *A)
 {
 	int mem, i;
@@ -72,3 +142,4 @@ void _swap(int a, int b, int *A)
 	if (A[a] != A[b])
 		print_array(A, i);
 }
+*/
